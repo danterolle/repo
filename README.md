818b132d8645c1c231176430d91bb700f88ef0b2
danterolle <danterolle@parrotsec.org>
2023-11-28 19:48:09 +0100

    add repo core file

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This repository is a reimplementation written in Python (3.11) of [the following
 
 It is all been redesigned and written again to cope with the new structural change in the ParrotOS repositories, particularly because of the Debian 12-based version 6.0.
 
+It works better, is better managed, and will be further improved.
+
 So, these scripts allow you to download the Packages file from any Debian repository (including ParrotOS), format it as needed to get a JSON format easily usable in the web.
---
+++ repo_downloader.py
@@ -0,0 +1,101 @@
+# This script is a CLI tool designed to extract the Parrot Packages files.
+
+# The Packages files typically contain details about Debian packages, such as their names, versions,
+# maintainer information, architecture, and various other package attributes.
+
+# The primary purpose of this tool is to selectively download Packages files from a specified Parrot
+# repository based on user-defined parameters, including codenames, branches, and architectures.
+# The downloaded Packages files are then organized into a folder structure that reflects 
+# the codename, branch, and architecture.
+
+import os
+import requests
+import argparse
+from tqdm import tqdm
+
+"""
+download_packages: Download Packages files based on user selection.
+
+Args:
+    - base_url (str): The base URL of the Debian repository.
+    - repo_config (dict): Configuration of valid codenames, branches, and architectures.
+    - selected_codenames (list): User-selected codenames.
+    - selected_branches (list): User-selected branches.
+    - selected_architectures (list): User-selected architectures.
+"""
+def download_packages(base_url, repo_config, selected_codenames, selected_branches, selected_architectures):
+
+    # Count the total number of iterations for tqdm
+    total_iterations = len(selected_codenames) * len(selected_branches) * len(selected_architectures)
+
+    # Use tqdm to display an overall progress bar
+    with tqdm(total=total_iterations, desc="Downloading Packages", unit="package") as pbar:
+        # Iterate over selected codenames
+        for codename in selected_codenames:
+            if codename not in repo_config["codenames"]:
+                print(f"Ignoring invalid codename: {codename}")
+                continue
+
+            # Iterate over selected branches
+            for branch in selected_branches:
+                if branch not in repo_config["branches"]:
+                    print(f"Ignoring invalid branch: {branch}")
+                    continue
+
+                # Iterate over selected architectures
+                for arch in selected_architectures:
+                    if arch not in repo_config["architectures"]:
+                        print(f"Ignoring invalid architecture: {arch}")
+                        continue
+
+                    # Build the URL for the Packages file
+                    url = f"{base_url}{codename}/{branch}/{arch}/Packages"
+                    # Define the local path where the Packages file will be saved
+                    download_path = f"{codename}/{branch}/{arch}/Packages"
+
+                    # Create the folder structure if it doesn't exist
+                    os.makedirs(os.path.dirname(download_path), exist_ok=True)
+
+                    print(f"Downloading {url} to {download_path}")
+
+                    # Perform the GET request to download the Packages file
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        # Save the downloaded content to the local file
+                        with open(download_path, "wb") as file:
+                            file.write(response.content)
+                        print(f"Download successful\n")
+                    else:
+                        print(f"Failed to download {url}\n")
+
+                    # Update the tqdm progress bar
+                    pbar.update(1)
+
+def main():
+    # Set up command-line argument parsing
+    parser = argparse.ArgumentParser(description="Download Parrot Packages files from a specified repository.")
+    parser.add_argument("--base-url", default="https://deb.parrot.sh/parrot/dists/", help="Specify a custom base URL.")
+    parser.add_argument("--codename", nargs="+", help="Specify codenames to download Packages for.")
+    parser.add_argument("--branch", nargs="+", help="Specify branches to download Packages for.")
+    parser.add_argument("--architecture", nargs="+", help="Specify architectures to download Packages for.")
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Repository configuration
+    repo_config = {
+        "codenames": ["lory", "lory-backports", "lory-updates", "lory-security"],
+        "branches": ["main", "contrib", "non-free", "non-free-firmware"],
+        "architectures": ["binary-amd64", "binary-arm64", "binary-armhf", "binary-i386"]
+    }
+
+    # Use selected values or default values from the repository config
+    selected_codenames = args.codenames or repo_config["codenames"]
+    selected_branches = args.branches or repo_config["branches"]
+    selected_architectures = args.architectures or repo_config["architectures"]
+
+    # Call the function to download Packages files
+    download_packages(args.base_url, repo_config, selected_codenames, selected_branches, selected_architectures)
+
+if __name__ == "__main__":
+    main()
